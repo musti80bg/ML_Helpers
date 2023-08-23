002fd7e0b0868f1abab95448e0846e05cdcd0c40
musti80bg <74068050+musti80bg@users.noreply.github.com>
2023-08-23 13:19:01 +0100

    minor fix

--- load_data_helpers.py
+++ load_data_helpers.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-def load_house_data():
-    data = np.loadtxt("./data/houses.txt", delimiter=',', skiprows=1)
+def load_house_data(path_to_data = "data"):
+    full_path = f"./{path_to_data}/houses.txt"
+    data = np.loadtxt(full_path, delimiter=',', skiprows=1)
     X = data[:,:4]
     y = data[:,4]
     return X, y
